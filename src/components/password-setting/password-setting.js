add556b948bdb58b7dea46b476ccc78ecf0916ef
Janushankan <vj.janushankan1006@gmail.com>
2024-01-30 16:10:11 +0530

    PasswordSettingsChanges

--- src/components/password-setting/password-setting.js
+++ src/components/password-setting/password-setting.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import formHandler from "../../utils/FormHandler";
 import { validateTaskPasswordSettings } from "../../utils/validation";
 import axios from "axios";
-// import { getUserId } from "../../utils/Authentication";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { setUserDetail, toggleLoader } from "../../redux/actions";
@@ -11,36 +10,37 @@ import Cookies from "js-cookie";
 import SettingImage from "../../assets/settings.svg";
 
 function PasswordSetting(props) {
-
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const [inputs, setInputs] = useState({});
-    const [submit, setSubmit] = useState(0);
     const [error, setError] = useState(null);
 
-
     const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
+        const { name, value } = event.target;
+        setInputs(prevState => ({ ...prevState, [name]: value }));
     }
 
     const handleSubmit = (event) => {
-        const finalInput = inputs
         event.preventDefault();
-        if (validateTaskPasswordSettings(finalInput)) {
-            passwordChange(finalInput)
-            setInputs({})
+        const { oldpassword, password, confirmPassword } = inputs;
+
+        if (password !== confirmPassword) {
+            setError("New password and confirm password do not match");
         } else {
-            setError("not match")
-            setInputs({})
+            setError(null);
+            if (validateTaskPasswordSettings(inputs)) {
+                passwordChange(inputs);
+                setInputs({});
+            } else {
+                setError("Validation failed");
+                setInputs({});
+            }
         }
     }
 
     function passwordChange(inputs) {
         dispatch(toggleLoader(true));
-        //const token = '079b662f7b61bbba9a013d040c0e7a8245fb3512320ea5c6b7f578e22f90c80c'
         const token = Cookies.get('tokenCookie');
         const config = {
             headers: {
@@ -50,40 +50,38 @@ function PasswordSetting(props) {
         axios.put('http://127.0.0.1:8000/user/', inputs, config)
             .then((res) => {
                 console.log(res)
-                if (res.status == 200) {
-                    toast.success("New Password Updated")
+                if (res.status === 200) {
+                    toast.success("New Password Updated");
                 }
-
-            }).catch((err) => {
-                if (err.response.status == 400) {
-                    toast.error("Incorrect Old Password")
-                } else if (err.response.status == 401) {
-                    toast.error("Unauthorized")
-                    //logout
-                    navigate("/login")
-
-                } else {
-                    toast.error("Incorrect")
-                }
-            }).finally(() => {
-                dispatch(toggleLoader(false));
             })
+            .catch((err) => {
+                if (err.response.status === 400) {
+                    toast.error("Incorrect Old Password");
+                } else if (err.response.status === 401) {
+                    toast.error("Unauthorized");
+                    navigate("/login");
+                } else {
+                    toast.error("Incorrect");
+                }
+            })
+            .finally(() => {
+                dispatch(toggleLoader(false));
+            });
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            setError(null)
+        const timer = setTimeout(() => {
+            setError(null);
         }, 1500);
-    }, [error])
+        return () => clearTimeout(timer);
+    }, [error]);
 
     return (
         <div className="row">
             <div className={'col-md-6 pt-3'}>
                 <div className="d-flex align-items-center justify-content-center">
                     <img
-                        className={
-                            "login-image img-responsive d-block align-items-center"
-                        }
+                        className={"login-image img-responsive d-block align-items-center"}
                         src={SettingImage}
                         alt=""
                     />
@@ -135,7 +133,7 @@ function PasswordSetting(props) {
                                     onChange={handleChange}
                                     required={true}
                                 />
-                                {<p className="text-red">{error}</p>}
+                                {error && <p className="text-red">{error}</p>}
                             </div>
                         </div>
                         <div className={"d-flex justify-content-end"}>
